9ca421a9a0f3f594e297ae6596e072bbc1ede622
MarcoRMIT <168316921+MarcoRMIT@users.noreply.github.com>
2025-04-09 01:20:34 +1000

    portfolio updated

---
+++ DESIGNPORTFOLIO/script.js
@@ -0,0 +1,6 @@
+// script.js
+document.querySelectorAll(".icon-button").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log("Social icon clicked!"); // For debugging or analytics
+  });
+});
