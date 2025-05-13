d693bab6f869cd5b5fc831f4781c1d11c5a54165
MarcoRMIT <168316921+MarcoRMIT@users.noreply.github.com>
2025-05-13 12:26:09 +1000

    week 10 updated

---
+++ week10/script.js
@@ -0,0 +1,21 @@
+document.querySelector("#hoverclick-button").addEventListener("click", () => {
+  location.href = "flip.html";
+});
+
+document.querySelector("#dragdrop-button").addEventListener("click", () => {
+  location.href = "dragdrop.html";
+});
+
+document
+  .querySelector("#multidragdrop-button")
+  .addEventListener("click", () => {
+    location.href = "multidragdrop.html";
+  });
+
+document.querySelector("#findqueen-button").addEventListener("click", () => {
+  location.href = "findqueen.html";
+});
+
+document.querySelector("#completegame-button").addEventListener("click", () => {
+  location.href = "game.html";
+});
